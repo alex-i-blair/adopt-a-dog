b6cdde0def8cd644fb836d757977c7e42009f753
Alex Blair <bclan11@gmail.com>
2021-12-13 14:27:40 -0800

    all go

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const dogListContainer = document.getElementById('dog-list-container');
 // on load
 // fetch all dogs
 // render and append all dog cards to the container
+window.addEventListener('load', async() => {
+    const dogs = await getDogs();
+
+    for (let dog of dogs) {
+        const dogCard = renderDogCard(dog);
+        dogListContainer.append(dogCard);
+
+    }
+});
--- detail/detail.js
+++ detail/detail.js
@@ -5,5 +5,12 @@ const dogDetailContainer = document.getElementById('dog-detail-container');
 
 // on load
 // get the id from URL
+window.addEventListener('load', async() => {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+    const dog = await getDog(id);
+    const dogCard = renderDogDetail(dog);
+    dogDetailContainer.append(dogCard);
+});
 // use the id to fetch the dog
 // render and append this dog's details to the container
